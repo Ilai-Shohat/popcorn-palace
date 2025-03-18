dcab0f153b2962ba0b8831bf399dd6d200da3527
ilai.shohat <ilai.shohat@post.runi.ac.il>
2025-03-18 21:11:38 +0200

    Add @Repository annotation to TicketRepository interface

--- src/main/java/com/att/tdp/popcorn_palace/repository/TicketRepository.java
+++ src/main/java/com/att/tdp/popcorn_palace/repository/TicketRepository.java
@@ -1,9 +1,11 @@
 package com.att.tdp.popcorn_palace.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.att.tdp.popcorn_palace.model.Ticket;
 
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
 }
