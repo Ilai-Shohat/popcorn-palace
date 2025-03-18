e860b75dd1ff80884da0f41f7e35e556830b1ebd
ilai.shohat <ilai.shohat@post.runi.ac.il>
2025-03-18 17:56:23 +0200

    Modified movie and showtime model to fit with JPA requirements

--- src/main/java/com/att/tdp/popcorn_palace/model/Movie.java
+++ src/main/java/com/att/tdp/popcorn_palace/model/Movie.java
@@ -1,16 +1,40 @@
 package com.att.tdp.popcorn_palace.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 @Entity
+@Table(name = "movies")
 public class Movie {
-    String title;
-    String genre;
-    int duration;
-    double rating;
-    int release_year;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "duration")
+    private int duration;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "release_year")
+    private int release_year;
+
+    public Movie() {
+    }
 
     public Movie(String title, String genre, int duration, double rating, int release_year) {
+        super();
         this.title = title;
         this.genre = genre;
         this.duration = duration;
--- src/main/java/com/att/tdp/popcorn_palace/model/Showtime.java
+++ src/main/java/com/att/tdp/popcorn_palace/model/Showtime.java
@@ -2,17 +2,40 @@ package com.att.tdp.popcorn_palace.model;
 
 import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "showtimes")
 public class Showtime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     Movie movie;
+
+    @Column(name = "theater")
     String theater;
+
+    @Column(name = "start_time")
     Date start_time;
+
+    @Column(name = "end_time")
     Date end_time;
+
+    @Column(name = "price")
     double price;
 
+    public Showtime() {
+    }
+
     public Showtime(Movie movie, String theater, Date start_time, Date end_time, double price) {
+        super();
         this.movie = movie;
         this.theater = theater;
         this.start_time = start_time;
