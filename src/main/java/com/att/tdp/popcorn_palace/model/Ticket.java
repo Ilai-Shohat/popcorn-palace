d155dd88011d66ef88e32d556087f9f5581c0f29
ilai.shohat <ilai.shohat@post.runi.ac.il>
2025-03-18 21:11:08 +0200

    Implemented basic ticket model

--- src/main/java/com/att/tdp/popcorn_palace/model/Ticket.java
+++ src/main/java/com/att/tdp/popcorn_palace/model/Ticket.java
@@ -1,8 +1,73 @@
 package com.att.tdp.popcorn_palace.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tickets")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "showtime_id")
+    private String showtimeId;
+
+    @Column(name = "seat_number")
+    private String seatNumber;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    public Ticket() {
+    }
+
+    public Ticket(String showtimeId, String seatNumber, String userId) {
+        super();
+        this.showtimeId = showtimeId;
+        this.seatNumber = seatNumber;
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShowtimeId() {
+        return showtimeId;
+    }
+
+    public void setShowtimeId(String showtimeId) {
+        this.showtimeId = showtimeId;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket [id=" + id + ", showtimeId=" + showtimeId + ", seatNumber=" + seatNumber + ", userId=" + userId
+                + "]";
+    }
 }
