2c7c9b19be9b9a5f107fb7b38e6521204e0a0957
ilai.shohat <ilai.shohat@post.runi.ac.il>
2025-03-18 21:12:29 +0200

    Add getters, setters, and toString methods for Movie and Showtime models

--- src/main/java/com/att/tdp/popcorn_palace/model/Movie.java
+++ src/main/java/com/att/tdp/popcorn_palace/model/Movie.java
@@ -41,4 +41,58 @@ public class Movie {
         this.rating = rating;
         this.release_year = release_year;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie [id=" + id + ", title=" + title + ", genre=" + genre + ", duration=" + duration + ", rating="
+                + rating + ", release_year=" + release_year + "]";
+    }
 }
--- src/main/java/com/att/tdp/popcorn_palace/model/Showtime.java
+++ src/main/java/com/att/tdp/popcorn_palace/model/Showtime.java
@@ -42,4 +42,58 @@ public class Showtime {
         this.end_time = end_time;
         this.price = price;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public String getTheater() {
+        return theater;
+    }
+
+    public void setTheater(String theater) {
+        this.theater = theater;
+    }
+
+    public Date getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Date start_time) {
+        this.start_time = start_time;
+    }
+
+    public Date getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Date end_time) {
+        this.end_time = end_time;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Showtime [id=" + id + ", movie=" + movie + ", theater=" + theater + ", start_time=" + start_time
+                + ", end_time=" + end_time + ", price=" + price + "]";
+    }
 }
