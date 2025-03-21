a210b1e52d1dc6108b467cb834160561ca294624
ilai.shohat <ilai.shohat@post.runi.ac.il>
2025-03-21 22:51:31 +0200

    Refactor MovieMapper to use MapStruct for DTO mapping

--- src/main/java/com/att/tdp/popcorn_palace/mapper/MovieMapper.java
+++ src/main/java/com/att/tdp/popcorn_palace/mapper/MovieMapper.java
@@ -2,50 +2,13 @@ package com.att.tdp.popcorn_palace.mapper;
 
 import com.att.tdp.popcorn_palace.dto.MovieDTO;
 import com.att.tdp.popcorn_palace.model.Movie;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Component
-public class MovieMapper {
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MovieMapper {
 
-    /**
-     * Converts a Movie entity to a MovieDTO
-     * 
-     * @param movie The Movie entity to convert
-     * @return The corresponding MovieDTO
-     */
-    public MovieDTO toDTO(Movie movie) {
-        if (movie == null) {
-            return null;
-        }
+    MovieDTO toDTO(Movie movie);
 
-        return new MovieDTO(
-                movie.getId(),
-                movie.getTitle(),
-                movie.getGenre(),
-                movie.getDuration(),
-                movie.getRating(),
-                movie.getReleaseYear());
-    }
-
-    /**
-     * Converts a MovieDTO to a Movie entity
-     * 
-     * @param dto The MovieDTO to convert
-     * @return The corresponding Movie entity
-     */
-    public Movie fromDTO(MovieDTO dto) {
-        if (dto == null) {
-            return null;
-        }
-
-        Movie movie = new Movie();
-        movie.setId(dto.getId());
-        movie.setTitle(dto.getTitle());
-        movie.setGenre(dto.getGenre());
-        movie.setDuration(dto.getDuration());
-        movie.setRating(dto.getRating());
-        movie.setReleaseYear(dto.getReleaseYear());
-
-        return movie;
-    }
+    Movie fromDTO(MovieDTO dto);
 }
