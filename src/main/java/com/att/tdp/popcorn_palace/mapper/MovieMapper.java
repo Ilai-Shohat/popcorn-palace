e9b7392aca31390083b4ab6ccae9b67d4e687eec
ilai.shohat <ilai.shohat@post.runi.ac.il>
2025-03-22 17:46:18 +0200

    Remove unused ReportingPolicy from MovieMapper annotation

--- src/main/java/com/att/tdp/popcorn_palace/mapper/MovieMapper.java
+++ src/main/java/com/att/tdp/popcorn_palace/mapper/MovieMapper.java
@@ -3,11 +3,9 @@ package com.att.tdp.popcorn_palace.mapper;
 import com.att.tdp.popcorn_palace.dto.MovieDTO;
 import com.att.tdp.popcorn_palace.model.Movie;
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring")
 public interface MovieMapper {
-
     MovieDTO toDTO(Movie movie);
 
     Movie fromDTO(MovieDTO dto);
